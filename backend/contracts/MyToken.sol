822bb1043657aeefe68ea94c8e43a354003120d1
cecile hirschauer <cecile.h.75@gmail.com>
2024-08-02 18:12:37 +0200

    Install chainlink dependencies and add DataConsumerV3 contract

---
+++ backend/contracts/MyToken.sol
@@ -0,0 +1,7 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
